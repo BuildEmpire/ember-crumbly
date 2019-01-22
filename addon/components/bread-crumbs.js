bef3a989b8195f80b85948e55c012127c4ca1ea8
Richard Thompson <richard.thompson@buildempire.co.uk>
2019-01-22 13:45:13 +0000

    #BPP36FE-Adding-params. Fixed.

--- addon/components/bread-crumbs.js
+++ addon/components/bread-crumbs.js
@@ -25,6 +25,7 @@ export default Component.extend({
   tagName: 'ol',
   linkable: true,
   reverse: false,
+  queryParams: null,
   classNameBindings: ['breadCrumbClass'],
   hasBlock: bool('template').readOnly(),
   routing: service('-routing'),
