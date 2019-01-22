41bce71bbdea93b7e84b1380e236c2bf66be8ad6
Richard Thompson <richard.thompson@buildempire.co.uk>
2019-01-22 14:12:49 +0000

    ember-crumbly updating to params.

--- addon/components/bread-crumbs.js
+++ addon/components/bread-crumbs.js
@@ -25,7 +25,7 @@ export default Component.extend({
   tagName: 'ol',
   linkable: true,
   reverse: false,
-  queryParams: null,
+  params: null,
   classNameBindings: ['breadCrumbClass'],
   hasBlock: bool('template').readOnly(),
   routing: service('-routing'),
